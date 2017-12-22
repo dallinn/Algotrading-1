8b7042ca7be5fa480d9dafd43555a2ebfe95d3d6
dallinn <dallinn@users.noreply.github.com>
2017-12-22 13:21:54 -0700

    quandl doesnt provide real time pricing

--- at.py
+++ at.py
@@ -1,38 +1,15 @@
 from flask import Flask
 from flask import jsonify
-import json
-import requests
+import quandl
+from keys import QUANDL_API_KEY
+quandl.ApiConfig.api_key = QUANDL_API_KEY
 
 app = Flask(__name__)
-gapi = lambda stockName: "https://finance.google.com/finance?q=" + stockName + "&output=json"
 
 @app.route('/')
 def index():
-    url = gapi("GOOG")
-    req = requests.get(url)
-    res = json.loads(req.content[6:-2].decode('unicode_escape'))
-
-    stock = {}
-
-    stock['52wkHigh']               = res['hi52']
-    stock['52wkLow']                = res['lo52']
-    stock['beta']                   = res['beta']
-    stock['change']                 = res['c']
-    stock['dividendYield']          = res['dy']
-    stock['eps']                    = res['eps']
-    stock['exchange']               = res['e']
-    stock['exchangeOpen']           = res['eo']
-    stock['high']                   = res['hi']
-    stock['institutionalOwnership'] = res['instown']
-    stock['low']                    = res['lo']
-    stock['name']                   = res['name']
-    stock['open']                   = res['op']
-    stock['pe']                     = res['pe']
-    stock['percentChange']          = res['cp']
-    stock['price']                  = res['l']
-    stock['symbol']                 = res['t']
-
-    return jsonify(stock)
+    data = quandl.get('NSE/OIL', rows=5, returns='json')
+    return jsonify(data)
 
 if __name__ == "__main__":
     app.run()
